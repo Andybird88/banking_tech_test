f7ceb63a8cc211d0174bfc23bfdb13b9117fb90b
88Andybird <anbird88@gmail.com>
2022-01-05 10:36:28 +0000

    Added print statement fucntion and related tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,27 @@
 class Account
     attr_reader :balance
   
-  def initialize
-    @balance = 0
-  end
-
-  def deposit(num)
-    @balance += num
-  end
-
-  def withdraw(num)
-    @balance -= num
-  end
-
+    def initialize
+        @balance = 0
+        @statement_data = []
+      end
+    
+      def deposit(num, date)
+        @balance += num
+        @statement_data << [date, num, ' ||      ', @balance]
+      end
+    
+      def withdraw(num, date)
+        @balance -= num
+        @statement_data << [date, '      || ', num, @balance]
+      end
+    
+      def statement
+        puts '    Date   ||  Credit ||  Debit   || Balance '
+        @statement_data.each do |transaction|
+          puts " #{transaction[0]}||   #{transaction[1]}  #{transaction[2]}    || #{transaction[3]}  "
+        end
+      end
 
 
   
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,15 +8,33 @@ describe Account do
 
     it 'allows a deposit' do
         test_account = Account.new
-        test_account.deposit(500)
+        test_account.deposit(500, '01/01/2023')
         expect(test_account.balance).to eq(500)
     end
 
     it 'allows a withdrawl' do
         test_account = Account.new
-        test_account.deposit(500)
-        test_account.withdraw(250)
+        test_account.deposit(500, '01/01/2023')
+        test_account.withdraw(250, '02/01/2023')
         expect(test_account.balance).to eq(250)
       end
+
+      it 'can display correct number of transacitions on statement' do
+        test_account = Account.new
+        test_account.deposit(20, '01/01/2023')
+        test_account.deposit(30, '02/01/2023')
+        test_account.withdraw(40, '03/01/2023')
+        expect(test_account.statement.length).to eq(3)
+      end
+    
+      it 'calculates correct balance if transactions given in date order' do
+          test_account = Account.new
+          test_account.deposit(20, '01/01/2023')
+          test_account.deposit(30, '02/01/2023')
+          test_account.withdraw(40, '03/01/2023')
+          expect(test_account.balance).to eq(10)
+      end
+
+    
     
 end
